e826bce3c7e2085f0fd94bc01cd37cbcab2af66a
Rajat1213 <rajatyadav.cs19@aryacollege.in>
2022-04-28 21:54:10 +0530

    First comment

--- new.js
+++ new.js
@@ -3,6 +3,32 @@
     console.debug('debug')
 }, 2000);*/
 
-setInterval(() => {
+/*setInterval(() => {
     console.warn('hi')
-}, 2000);
+}, 2000);*/
+
+// let a = [100,200,10,1];
+// let b = a.filter((ele) => ele <= 10);
+// let c = b.sort()
+// console.log(b);
+
+// let a = [13,21,45,1];
+// let b = a.sort()
+// let c = b.pop(45)
+
+// console.log(b);
+
+// let a = [5,1,7,0];
+// let b = a.filter((ele) => ele <= 0);
+// console.log(b)
+
+
+// let a = [10,3,4,1,2];
+// let b = a.sort()
+// let c = b.filter((ele) => ele <=3);
+// console.log(c)
+
+let a = [20,13,14,11,12];
+let b = a.sort()
+let c = b.filter((ele) => ele > 12);
+console.log(c)
